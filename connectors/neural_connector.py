67d8fab6b9a2732cb1fb0b1ac37dcf7609c5ca32
lexter0705 <lexter0705@gmail.com>
2025-10-16 13:18:34 +0300

    1) Refaactored neural connector

--- connectors/neural_connector.py
+++ connectors/neural_connector.py
@@ -20,10 +20,12 @@ class NeuralConnector:
     async def send_file(self, task_file: str):
         self.__task_file = task_file
         video = cv2.VideoCapture(self.__task_file)
+        count_frames = 0
         while True:
             ret, frame = video.read()
             byte_frame = frame.tobytes()
-            await self.__current_socket.send_bytes(byte_frame)
+            data = {"frame": byte_frame, "frame_id": count_frames}
+            await self.__current_socket.send_json(data)
             answer = await self.__current_socket.receive_json()
             if answer is not None:
                 data = AnalystData.model_validate_json(answer)
@@ -32,5 +34,6 @@ class NeuralConnector:
             if not ret:
                 break
             await asyncio.sleep(0.1)
+            count_frames += 1
 
         self.__task_file = None
