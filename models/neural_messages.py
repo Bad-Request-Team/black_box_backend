893594a98c71c4f5a5b771ec4713d9e3bd2a9564
lexter0705 <lexter0705@gmail.com>
2025-10-16 12:26:22 +0300

    1) Some

--- models/neural_messages.py
+++ models/neural_messages.py
@@ -2,12 +2,8 @@ from pydantic import BaseModel, confloat, conint
 
 
 class AnalystData(BaseModel):
-    avg_speed: confloat(ge=0)
-    aggressive_percent: confloat(ge=0, le=1)
-    drivers_count: conint(ge=0)
-    aggressive_drivers_count: conint(ge=0)
-    normal_drivers_count: conint(ge=0)
-    abrupt_braking_count: conint(ge=0)
-    abrupt_acceleration_count: conint(ge=0)
-    max_speed: confloat(ge=0)
-    min_speed: confloat(ge=0)
+    total_count: int
+    aggressive_count: int
+    acc_count: int 
+    br_count: int
+    lane_count: int
